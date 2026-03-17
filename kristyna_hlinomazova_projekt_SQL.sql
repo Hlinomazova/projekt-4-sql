020dabf3a13ec2ff42dedfccbabc0ceeb5ba7e9e
Kristýna Hlinomazová <k.hlinomazova@gmail.com>
2026-03-17 15:24:43 +0100

    Update kristyna_hlinomazova_projekt_SQL.sql

--- kristyna_hlinomazova_projekt_SQL.sql
+++ kristyna_hlinomazova_projekt_SQL.sql
@@ -1,17 +1,17 @@
 -- 1. TVORBA PRIMÁRNÍ TABULKY 
 CREATE TABLE t_kristyna_hlinomazova_project_SQL_primary_final AS
 WITH payroll_data AS (
-   -- První CTE: mzdy podle odvětví a roků
-   SELECT
-       payroll_year,
-       cpib.name AS industry_name,
-       ROUND(AVG(cp.value)) AS avg_month_salary_czk
-   FROM czechia_payroll AS cp
-   JOIN czechia_payroll_industry_branch AS cpib
-       ON cp.industry_branch_code = cpib.code
-   WHERE cp.value_type_code = 5958
+	První CTE: mzdy podle odvětví a roků
+	SELECT
+		payroll_year,
+    	cpib.name AS industry_name,
+    	ROUND(AVG(cp.value)) AS avg_month_salary_czk
+	FROM czechia_payroll AS cp
+	JOIN czechia_payroll_industry_branch AS cpib
+		ON cp.industry_branch_code = cpib.code
+	WHERE cp.value_type_code = 5958
 		AND cp.calculation_code = 200
-   GROUP BY payroll_year, industry_name
+	GROUP BY payroll_year, industry_name
 ),
 price_data AS (
    -- Druhé CTE: průměrné ceny potravin podle roků
