0aac9f753436ea31b7ec347c89a1d9a0d2db2179
Kristýna Hlinomazová <k.hlinomazova@gmail.com>
2026-03-18 15:18:05 +0100

    Create kristyna_hlinomazova_projekt_sql_q3.sql

---
+++ kristyna_hlinomazova_projekt_sql_q3.sql
@@ -0,0 +1,23 @@
+-- Q3: Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)? 
+WITH price_tables AS (
+	SELECT
+		payroll_year, 
+		food_category,
+		AVG(avg_price_czk) AS avg_price_current_year,
+		LAG(AVG(avg_price_czk)) OVER (PARTITION BY food_category ORDER BY payroll_year) AS avg_price_previous_yea
+	FROM t_kristyna_hlinomazova_project_sql_primary_final
+	GROUP BY payroll_year, food_category
+),
+growth_calc AS (
+	SELECT
+		food_category,
+		ROUND((avg_price_current_year / avg_price_previous_year - 1) * 100, 2) AS price_growth_percent
+	FROM price_tables
+)
+SELECT
+	food_category,
+	ROUND(AVG(price_growth_percent), 2) AS avg_yearly_growth_pct
+FROM growth_calc
+WHERE price_growth_percent IS NOT NULL
+GROUP BY food_category
+ORDER BY avg_yearly_growth_pct ASC;
