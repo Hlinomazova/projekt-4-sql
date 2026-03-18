18d743e9324c913f4238e9238c66f6b5b15a53f3
Kristýna Hlinomazová <k.hlinomazova@gmail.com>
2026-03-18 15:12:07 +0100

    Create t_kristyna_hlinomazova_project_SQL_secondary_final.sql

---
+++ t_kristyna_hlinomazova_project_SQL_secondary_final.sql
@@ -0,0 +1,13 @@
+-- 2. TVORBA SEKUNDÁRNÍ TABULKY 
+CREATE TABLE t_kristyna_hlinomazova_project_SQL_secondary_final AS
+SELECT
+	e.year,
+	c.country,
+	ROUND(e.population) AS population,
+	ROUND(e.gdp::numeric, 0) AS gdp,
+	ROUND(e.gini::numeric, 1) AS gini
+FROM countries AS c
+JOIN economies AS e
+	ON c.country = e.country
+WHERE c.continent = 'Europe'
+	AND e.year BETWEEN 2006 AND 2018; -- Aby to sedělo k mzdám v ČR
